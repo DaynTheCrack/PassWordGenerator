0d5229470418440f8022ceee7adc19170bcb32c0
Alexandre <88147150+DaynTheCrack@users.noreply.github.com>
2022-04-03 22:32:03 +0200

    Add files via upload

---
+++ PassWordGenerator/PassWordGenerator.py
@@ -0,0 +1,24 @@
+# password generator hard to break
+import random
+from numpy import number
+
+from pikepdf import PasswordError
+lower_letters ="azertyuiopqsdfghjklmwxcvbn"
+
+upper_letters = "AZERTYUIOPQSDFGHJKLMWXCVBN"
+
+numbers = "1234567890"
+
+symbols = "|[{#~&-`\_^@])°}+=¨$£¤%*µ!:§:/;.,?"
+
+spe_letter = "éà"
+
+#assemblage de tout
+use_for = lower_letters + upper_letters + numbers + symbols + spe_letter
+nombre = int(input("Nombre de caractères ?\n>>>"))
+if type(nombre) is not int:
+    raise ValueError("Tu dois rentrer un nombre !")
+else:
+    password ="".join(random.sample(use_for,nombre))
+    dict_password = {"password" : password}
+print(dict_password)
